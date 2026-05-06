cb230568285a642fc7d4d9a61167fc3c37af748e
banothuvinodkumar <vinodkumarbanothu855@gmail.com>
2026-05-06 06:44:35 +0530

    2nd cmt

--- client/src/api/axios.js
+++ client/src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const API = axios.create({
-    baseURL: import.meta.env.VITE_API_URL || 'http://localhost:5000/api',
+    baseURL: import.meta.env.VITE_API_URL || (import.meta.env.DEV ? 'http://localhost:5000/api' : 'https://blog-app-er1z.onrender.com/api'),
 });
 
 // Intercept requests to add the JWT token securely
